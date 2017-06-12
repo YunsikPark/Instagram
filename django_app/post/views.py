766fbbc1d76e5461331e6919d609ad2a46a5b926
Yunsik Park <space214@gmail.com>
2017-06-12 15:34:09 +0900

    post_list views추가

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    # 모든 Post목록을 'post'라는 key로 context에 담아 return render 처리
+    # post/post_list.html을 template으로 사용하도록 한다.
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'post/post_list.html', context=context)
