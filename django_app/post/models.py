85ae880065ca888ef29620deee0dd1b85faf1088
Yunsik Park <space214@gmail.com>
2017-06-12 11:27:24 +0900

    Post, Comment, Tag모델 생성

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 """
 member application 생성
@@ -9,11 +10,31 @@ member application 생성
 
 
 class Post(models.Model):
-    pass
+    # 장고가 제공하는 기본 User와 연결되도록 수정
+    author = models.ForeignKey(User)
+    photo = models.ImageField(blank=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
+    like_users = models.ManyToManyField(
+        User,
+        related_name='like_posts'
+    )
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return '{} ({} / {})'.format(
+            self.author,
+            self.create_date,
+            self.modified_date
+        )
 
 
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(User)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
 
 
 class PostLike(models.Model):
@@ -21,4 +42,7 @@ class PostLike(models.Model):
 
 
 class Tag(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Tag({})'.format(self.name)
