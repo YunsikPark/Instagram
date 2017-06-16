de1778febb8ac61d0ab0bd4e0a9dd28c7151bdb8
Yunsik Park <space214@gmail.com>
2017-06-16 12:56:41 +0900

    signup view, template, form, form username validation, css구현

---
+++ django_app/member/forms/signup.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class SignupForm(forms.Form):
+    # SignupForm을 구성하고 해당 form을 view에서 사용하도록 설정
+    username = forms.CharField(
+        max_length=20,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': '사용할 아이디를 입력하세요',
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': '비밀번호를 입력하세요',
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': '비밀번호를 다시한번 입력하세요',
+            }
+        )
+    )
+
+    # clean_<fieldname>메서드를 사용해서
+    # username필드에 대한 유효성 검증을 실행
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exist():
+            raise forms.ValidationError(
+                'Username already exist'
+            )
+        return username
