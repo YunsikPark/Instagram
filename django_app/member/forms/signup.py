eddfd34d7739fdea0d357731aa56aeb8c13282b7
Yunsik Park <space214@gmail.com>
2017-06-16 15:43:17 +0900

    SignupForm사용

--- django_app/member/forms/signup.py
+++ django_app/member/forms/signup.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 
 User = get_user_model()
 
@@ -32,9 +33,30 @@ class SignupForm(forms.Form):
     # clean_<fieldname>메서드를 사용해서
     # username필드에 대한 유효성 검증을 실행
     def clean_username(self):
-        username = self.cleaned_data['username']
-        if User.objects.filter(username=username).exists():
+        username = self.cleaned_data.get('username')
+        if username and User.objects.filter(username=username).exists():
             raise forms.ValidationError(
                 'Username already exist'
             )
         return username
+
+    def clean_password2(self):
+        # password1과 비교하여 같은지
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                'Password mismatched',
+            )
+        return password2
+
+    def create_user(self):
+        # 자신의 cleaned_data를 사용해서 유저를 생성
+        # 생성한 유저를 반환
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password2']
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+        return user
